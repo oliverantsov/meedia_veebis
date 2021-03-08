bd90442d3bab08ed6565566815db572f5dc7007c
Oliver Antsov <oliverantsov@users.noreply.github.com>
2021-03-08 20:48:28 +0200

    5th week (homework done).

--- tund_5/javascript/script1.js
+++ tund_5/javascript/script1.js
@@ -1,9 +1,9 @@
 let message = "Töötab!"; //varem oli var
-let picurl = "../../../~rinde/media/photos/TLU_600x400/";
-let picnameprefix = "tlu_";
-let picext = ".jpg";
-let minpicnum = 1;
-let maxpicnum = 43;
+let picUrl = "../../../~rinde/media/photos/TLU_600x400/";
+let picNamePrefix = "tlu_";
+let picExt = ".jpg";
+let minPicNum = 1;
+let maxPicNum = 43;
 
 window.onload = function() {
     //alert(message);
@@ -15,25 +15,29 @@ window.onload = function() {
 }
 
 function putOpenTime() {
-    let currenttime = new Date();
-    let currenthour = currenttime.getHours();
-    let currentminute = currenttime.getMinutes();
-    let currentsecond = currenttime.getSeconds();
-    document.getElementById("open_message").innerHTML = "Leht avati kell " + currenthour + ":" + currentminute + ":" + currentsecond + ".";
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds();
+    document.getElementById("open_message").innerHTML = "Leht avati kell " + currentHour + ":" + currentMinute + ":" + currentSecond + ".";
 }
 
 function putRandomPic() {
-    let randomnum = minpicnum + Math.round(Math.random() * (maxpicnum - minpicnum));
-    document.getElementById("tlu_pic").src = picurl + picnameprefix + randomnum + picext;
+    let randomNum = minPicNum + Math.round(Math.random() * (maxPicNum - minPicNum));
+    document.getElementById("tlu_pic").src = picUrl + picNamePrefix + randomNum + picExt;
 }
 
 function clockTick() {
-    let currenttime = new Date();
-    let currenthour = currenttime.getHours();
-    let currentminute = currenttime.getMinutes();
-    let currentsecond = currenttime.getSeconds();
-    let secangle = currentsecond * 6;
-    document.getElementById("secondhand").style.transform = "rotate(" + secangle + "deg)";
+    let currentTime = new Date();
+    let currentHour = currentTime.getHours();
+    let currentMinute = currentTime.getMinutes();
+    let currentSecond = currentTime.getSeconds();
+    let secAngle = currentSecond * 6;
+    let minAngle = currentMinute * 6 + (secAngle / 60);
+    let hourAngle = currentHour * 30 + ((currentMinute * 6) / 12);
+    document.getElementById("secondhand").style.transform = "rotate(" + secAngle + "deg)";
+    document.getElementById("minutehand").style.transform = "rotate(" + minAngle + "deg)";
+    document.getElementById("hourhand").style.transform = "rotate(" + hourAngle + "deg)";
     // setTimeout(1000,clockTick);
     requestAnimationFrame(clockTick);
 }
